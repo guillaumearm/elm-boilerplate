389823abe9b8260d01d637c37362e8e16ef692d2
Guillaume ARM <trapcodien@gmail.com>
2017-09-24 14:44:55 +0200

    elm-create.sh create a symbolic link instead of copy Makefile

--- elm-create.sh
+++ elm-create.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+ELM_BOILERPLATE_DIR=~/trap/elm/boilerplate
+
 function error() {
   >&2 echo $*
   exit 1
@@ -9,7 +11,7 @@ function error() {
 
 [ "$(/usr/bin/env ls -A)" != "" ] && error Error: current folder should be empty
 
-cp ~/trap/elm/boilerplate/Makefile . || error Error: Makefile not found
+ln -s $ELM_BOILERPLATE_DIR/Makefile . || error Error: Makefile not found
 
 echo Creating app : $*
 APP_TITLE="$*" make
